87b34e246c2ee64801cc44332fee991c2fad6990
Ethan Lee <dalpong2014@gmail.com>
2022-11-13 11:08:54 -0500

    login page and about us page in shape

--- client/features/About/About.js
+++ client/features/About/About.js
@@ -1,14 +1,63 @@
 import React from "react";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 function About() {
   return (
     <div>
       <h1 id="about-us-title">About Us</h1>
       <div className="about-us-container">
-        <div>Emmanuel Cruz</div>
-        <div>John-David Noguera</div>
-        <div>Alejandro Baez</div>
-        <div>Ethan Lee</div>
+        <div className="left-margin">
+          <div>Emmanuel Cruz</div>
+          <img src="" />
+          <span> [Profile pic goes here]</span>
+          <p className="paragraph">
+            About you. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque luctus id ligula non pellentesque.
+          </p>
+          <div>
+            {" "}
+            <FaLinkedin /> <FaGithub />{" "}
+          </div>
+        </div>
+        <div className="middle-margin">
+          <div>John-David Noguera</div>
+          <img src="" />
+          <span> [Profile pic goes here]</span>
+          <p className="paragraph">
+            About you. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque luctus id ligula non pellentesque.
+          </p>
+          <div>
+            {" "}
+            <FaLinkedin /> <FaGithub />{" "}
+          </div>
+        </div>
+        <div className="middle-margin">
+          <div>Alejandro Baez</div>
+          <img src="" />
+          <span> [Profile pic goes here]</span>
+          <p className="paragraph">
+            About you. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque luctus id ligula non pellentesque.
+          </p>
+          <div>
+            {" "}
+            <FaLinkedin /> <FaGithub />{" "}
+          </div>
+        </div>
+        <div className="right-margin">
+          <div>Ethan Lee</div>
+          <img src="" />
+          <span> [Profile pic goes here]</span>
+          <p className="paragraph">
+            About you. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque luctus id ligula non pellentesque.
+          </p>
+          <div>
+            {" "}
+            <FaLinkedin /> <FaGithub />{" "}
+          </div>
+        </div>
       </div>
     </div>
   );

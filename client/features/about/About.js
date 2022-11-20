bb41796328ffb8e339a4a19200f7b55f57eba323
John-David Noguera <JNogueraD94@gmail.com>
2022-11-20 11:22:34 -0500

    about us cleaned up

--- client/features/about/About.js
+++ client/features/about/About.js
@@ -3,23 +3,24 @@ import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 function About() {
   return (
-    <div className="bg-red-800">
-      <h1 id="about-us-title">About Us</h1>
+    <div class="bg-gray-300 m-10 p-10 flex-col items-center rounded-2xl">
+      <h1 class="text-center">About Us</h1>
+      <div class="text-center">Meet our skilled professionals</div>
       <div className="about-us-container">
-        <div className="left-margin">
+        <div className="custom-single-about">
           <div>Emmanuel Cruz</div>
           <img src="" />
           <span> [Profile pic goes here]</span>
-          <p className="bg-cyan-400 text-xl m-10">
+          <p className="paragraph">
             About you. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Pellentesque luctus id ligula non pellentesque.
           </p>
           <div>
-            {" "}
-            <FaLinkedin /> <FaGithub />{" "}
+            <FaLinkedin />
+            <FaGithub />
           </div>
         </div>
-        <div className="middle-margin">
+        <div className="custom-single-about">
           <div>John-David Noguera</div>
           <img src="" />
           <span> [Profile pic goes here]</span>
@@ -32,7 +33,7 @@ function About() {
             <FaLinkedin /> <FaGithub />{" "}
           </div>
         </div>
-        <div className="middle-margin">
+        <div className="custom-single-about">
           <div>Alejandro Baez</div>
           <img src="" />
           <span> [Profile pic goes here]</span>
@@ -45,7 +46,7 @@ function About() {
             <FaLinkedin /> <FaGithub />{" "}
           </div>
         </div>
-        <div className="right-margin">
+        <div className="custom-single-about">
           <div>Ethan Lee</div>
           <img src="" />
           <span> [Profile pic goes here]</span>

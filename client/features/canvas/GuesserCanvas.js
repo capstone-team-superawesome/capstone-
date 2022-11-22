3ab0238af90211f14fc92e5e862e67b1ad86c770
Alejandro Baez <abaez2623@gmail.com>
2022-11-22 16:35:13 -0500

    Fixed canvas issue

--- client/features/canvas/GuesserCanvas.js
+++ client/features/canvas/GuesserCanvas.js
@@ -1,38 +1,44 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-const GuesserCanvas = ({canvasRef}) => {
+const GuesserCanvas = ({ canvasRef }) => {
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = "1000";
+    canvas.height = "500";
+  }, []);
+
   return (
     <div>
-    <div
-      style={{
-        fontWeight: "bold",
-        textAlign: "center",
-        fontSize: "32px",
-      }}
-    >
-      You are guessing
-    </div>
-    <div
-      className="canvas-wrapper"
-      style={{ cursor: "not-allowed", pointerEvents: "none" }}
-    >
-      <canvas
-        id="container-canvas"
-        ref={canvasRef}
+      <div
         style={{
-          border: "2px solid black",
-          paddingLeft: "0",
-          paddingRight: "0",
-          marginLeft: "auto",
-          marginRight: "auto",
-          display: "block",
+          fontWeight: "bold",
+          textAlign: "center",
+          fontSize: "32px",
         }}
-      />
+      >
+        You are guessing
+      </div>
+      <div
+        className="canvas-wrapper"
+        style={{ cursor: "not-allowed", pointerEvents: "none" }}
+      >
+        <canvas
+          id="container-canvas"
+          ref={canvasRef}
+          style={{
+            border: "2px solid black",
+            paddingLeft: "0",
+            paddingRight: "0",
+            marginLeft: "auto",
+            marginRight: "auto",
+            display: "block",
+          }}
+        />
+      </div>
+      <input type="text" placeholder="make a guess"></input>
+      <button type="submit">Submit</button>
     </div>
-    <input type="text" placeholder="make a guess"></input>
-    <button type="submit">Submit</button>
-  </div>
-  )
-}
+  );
+};
 
-export default GuesserCanvas
+export default GuesserCanvas;

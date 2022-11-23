3eebb8e7b2303a3e8d6cf5c943a2691f5cfdd97d
Emmanuel <51297552+emmanuelcruz5@users.noreply.github.com>
2022-11-23 13:10:11 -0500

    gameSession model created

--- server/db/models/GameSession.js
+++ server/db/models/GameSession.js
@@ -2,9 +2,17 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 
 const GameSession = db.define("gamesession", {
+  gameCode: {
+    type: Sequelize.STRING,
+    isUnique: true,
+  },
   isInSession: {
     type: Sequelize.BOOLEAN,
     defaultValue: false,
+  },
+  currentPrompt: {
+    type: Sequelize.STRING,
+    defaultValue: "sculpture",
   },
 });
 

a82d9c808bde0834a2a581e90a22c4e0efa5db04
John-David Noguera <JNogueraD94@gmail.com>
2022-11-26 17:28:40 -0500

    Landing page complete

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -28,38 +28,46 @@ const Home = (props) => {
   return (
     <div>
       <Landing />
-      <div
-        id="initialScreen"
-        class="bg-gray-300 my-10 mx-auto p-10 flex-col rounded-2xl w-1/2"
-      >
-        <div class="flex-col items-center justify-center gap-5">
-          <button
-            id="newGameButton"
-            class="w-full bg-blue-400 hover:bg-blue-500 text-white font-serif py-2  border-b-4 border-blue-700 hover:border-blue-500 rounded hover:shadow-lg hover:shadow-cyan-500"
-            onClick={() => handleCreateGame()}
-          >
-            Create a Room
-          </button>
-          <div class="text-center">OR</div>
-          <div>
+      {id ? (
+        <div id="initialScreen" class="mt-5 mx-auto w-3/4">
+          <div class="flex items-center justify-center gap-5">
+            <button
+              id="newGameButton"
+              class="w-full bg-blue-400 hover:bg-blue-500 text-white font-serif py-2  border-b-4 border-blue-700 hover:border-blue-500 rounded hover:shadow-lg hover:shadow-cyan-500"
+              onClick={() => handleCreateGame()}
+            >
+              Create a Room
+            </button>
+            <div class="text-center text-white font-serif text-xl">OR</div>
+
             <input
               type="text"
               placeholder="Enter Game Code"
-              class="w-full px-3 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              class="w-1/2 px-3 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
       focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
               value={inputGameCode}
               onChange={(e) => setInputGameCode(e.target.value)}
             />
+
+            <button
+              id="joinGameButton"
+              class="w-1/2 bg-blue-400 hover:bg-blue-500 text-white font-serif py-2  border-b-4 border-blue-700 hover:border-blue-500 rounded hover:shadow-lg hover:shadow-cyan-500"
+              onClick={() => handleJoinGame()}
+            >
+              Join Game
+            </button>
           </div>
+        </div>
+      ) : (
+        <div class="flex justify-center mt-5">
           <button
-            id="joinGameButton"
-            class="w-full bg-blue-400 hover:bg-blue-500 text-white font-serif py-2  border-b-4 border-blue-700 hover:border-blue-500 rounded hover:shadow-lg hover:shadow-cyan-500"
-            onClick={() => handleJoinGame()}
+            class="w-1/2 bg-blue-400 hover:bg-blue-500 text-white font-serif py-2  border-b-4 border-blue-700 hover:border-blue-500 rounded hover:shadow-lg hover:shadow-cyan-500"
+            onClick={() => navigate("/login")}
           >
-            Join Game
+            Login to Play!
           </button>
         </div>
-      </div>
+      )}
     </div>
   );
 };

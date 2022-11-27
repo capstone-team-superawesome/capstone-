180a7916e8f10c41a50e33f7d43bffa78966e943
John-David Noguera <JNogueraD94@gmail.com>
2022-11-27 12:25:46 -0500

    About us page color theme set

--- client/features/about/About.js
+++ client/features/about/About.js
@@ -3,15 +3,17 @@ import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 function About() {
   return (
-    <div class="bg-gray-300 m-10 p-10 flex-col items-center rounded-2xl">
-      <h1 class="text-center">About Us</h1>
-      <div class="text-center">Meet our skilled professionals</div>
-      <div className="about-us-container">
-        <div className="custom-single-about">
-          <div>Emmanuel Cruz</div>
+    <div class="bg-gradient-to-r from-yellow-400 via-amber-400 to-orange-400 m-10 p-10 flex-col items-center rounded-2xl">
+      <h1 class="text-center text-xl">About Us</h1>
+      <div class="text-center text-2xl">Meet our skilled professionals</div>
+      <div className="about-us-container" class="flex mt-24 space-x-4">
+        <div className="custom-single-about" class="m-2">
+          <div class="font-serif underline decoration-2 decoration-blue-400 underline-offset-4">
+            Emmanuel Cruz
+          </div>
           <img src="" />
           <span> [Profile pic goes here]</span>
-          <p className="paragraph">
+          <p className="paragraph" class="font-serif">
             About you. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Pellentesque luctus id ligula non pellentesque.
           </p>
@@ -20,11 +22,13 @@ function About() {
             <FaGithub />
           </div>
         </div>
-        <div className="custom-single-about">
-          <div>John-David Noguera</div>
+        <div className="custom-single-about" class="m-2">
+          <div class="font-serif underline decoration-2 decoration-blue-400 underline-offset-4">
+            John-David Noguera
+          </div>
           <img src="" />
           <span> [Profile pic goes here]</span>
-          <p className="paragraph">
+          <p className="paragraph" class="font-serif">
             About you. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Pellentesque luctus id ligula non pellentesque.
           </p>
@@ -33,11 +37,13 @@ function About() {
             <FaLinkedin /> <FaGithub />{" "}
           </div>
         </div>
-        <div className="custom-single-about">
-          <div>Alejandro Baez</div>
+        <div className="custom-single-about" class="m-2">
+          <div class="font-serif underline decoration-2 decoration-blue-400 underline-offset-4">
+            Alejandro Baez
+          </div>
           <img src="" />
           <span> [Profile pic goes here]</span>
-          <p className="paragraph">
+          <p className="paragraph" class="font-serif">
             About you. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Pellentesque luctus id ligula non pellentesque.
           </p>
@@ -46,11 +52,13 @@ function About() {
             <FaLinkedin /> <FaGithub />{" "}
           </div>
         </div>
-        <div className="custom-single-about">
-          <div>Ethan Lee</div>
+        <div className="custom-single-about" class="m-2">
+          <div class="font-serif underline decoration-2 decoration-blue-400 underline-offset-4">
+            Ethan Lee
+          </div>
           <img src="" />
           <span> [Profile pic goes here]</span>
-          <p className="paragraph">
+          <p className="paragraph" class="font-serif">
             About you. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Pellentesque luctus id ligula non pellentesque.
           </p>

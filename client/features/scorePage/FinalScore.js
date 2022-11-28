5fc7a26718d4e6c44ed59e7a71f23a6b07af7208
John-David Noguera <JNogueraD94@gmail.com>
2022-11-28 16:50:11 -0500

    scorePage updated with canva

--- client/features/scorePage/FinalScore.js
+++ client/features/scorePage/FinalScore.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const FinalScore = () => {
+  const navigate = useNavigate();
+
   return (
-    <div class="flex justify-center">
+    <div class="w-3/4 flex-col justify-center mx-auto my-20">
+      <button
+        class="w-1/2 bg-blue-400 hover:bg-blue-500 text-white font-serif py-2  border-b-4 border-blue-700 hover:border-blue-500 rounded hover:shadow-lg hover:shadow-cyan-500 mx-auto"
+        onClick={() => navigate("/home")}
+      >
+        Play again!
+      </button>
       <img class="w-full" src="img/Final-score-background.gif" />
-      finalScore
     </div>
   );
 };

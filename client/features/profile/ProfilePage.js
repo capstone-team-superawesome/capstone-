8e177488b6829a8a33725c82571fc879341ba30f
Emmanuel <51297552+emmanuelcruz5@users.noreply.github.com>
2023-01-08 03:13:05 -0500

    Progress Bar added

--- client/features/profile/ProfilePage.js
+++ client/features/profile/ProfilePage.js
@@ -19,6 +19,8 @@ const Profile = () => {
   //Level up and Ranking System. 10 level ups, each requires ~1.3x the experience of the last.
   let exp = 10000;
   let currentLvl = 0;
+  let nextLevel = 10000;
+  let barSize = 50;
   const levels = {};
 
   for (let i = 1; i <= 10; i++) {
@@ -31,13 +33,21 @@ const Profile = () => {
 
   for (let i = 0; i < levelsArray.length; i++) {
     if (totalScore >= levelsArray[i] && totalScore < levelsArray[i + 1]) {
-      currentLvl = levelsArray[i];
+      currentLvl = levels[levelsArray[i]];
+      nextLevel = levelsArray[i + 1];
+      barSize = Math.round(
+        ((totalScore - levelsArray[i]) /
+          (levelsArray[i + 1] - levelsArray[i])) *
+          100
+      );
       break;
     }
   }
 
   totalScore > lastLevel ? (currentLvl = levels[lastLevel]) : null;
-  //console.log(levels);
+  totalScore < levelsArray[0]
+    ? (barSize = Math.round((totalScore / levelsArray[0]) * 100))
+    : null;
   //
 
   return (
@@ -92,19 +102,30 @@ const Profile = () => {
       </div>
       <div class="flex-col justify-start w-1/3 mx-2 text-center">
         <div class="text-4xl">Level {currentLvl} </div>
-        <div>Level Up Bar</div>
-        <div class="text-4xl"> {totalScore} exp</div>
+        <div>
+          <div
+            style={{
+              margin: "auto",
+              width: "300px",
+              height: "15px",
+              overflow: "hidden",
+              borderRadius: "40px",
+              border: "black solid 2px",
+            }}
+          >
+            <div
+              style={{
+                backgroundColor: "#ffee42",
+                height: "100%",
+                width: `${barSize}%`,
+              }}
+            ></div>
+          </div>
+        </div>
+        <div class="text-4xl">
+          {totalScore}/{nextLevel} exp
+        </div>
       </div>
-      {/* Not sure what this is for */}
-      {/* <section
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "2%",
-        }}
-      >
-        <h1 style={{ fontSize: "250%" }}>Top Scores</h1>
-      </section> */}
     </div>
   );
 };
